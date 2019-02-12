92be805ac6779942069aeb82eee8eb6861fb1109
Cory Press <cmpress0107@my.mwsu.edu>
2019-02-12 01:03:41 -0600

    Update scrape_game_data.py

--- Assignments/A03/scrape_game_data.py
+++ Assignments/A03/scrape_game_data.py
@@ -24,28 +24,18 @@ for year in range (2009, 2019):
         url = "http://www.nfl.com/schedules/" + str(year) + "/REG" +str(week)
         page = scraper.go(url)
         
-        #depending on playtime? gameid is in diferent class
-        #  and 2009 has complete different classes
-        divs = page.find_all('div',{"class":"schedules-list-content post expandable primetime type-reg pro-legacy"})
-        divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable type-reg pro-legacy"}))
-        divs.extend( page.find_all('div',{"class":"schedules-list-content post primetime type-reg pro-legacy"}))
-        divs.extend( page.find_all('div',{"class":"schedules-list-content post type-reg pro-legacy"}))
+        #get all game classes
+        divs = page.find_all('div',{"class":"schedules-list-content"})
         
+        #get gameid from those clases
         for div in divs:
             games += 1
             gameids.append(div['data-gameid'])
             
-            
+
 #
 # POSTSEASON
 #
-
-#get ids for just 2009 so i dont waste time searching for
-#classes in other years that they dont have
-url = "http://www.nfl.com/schedules/2009/POST" 
-page = scraper.go(url)
-
-
 
 for year in range (2009, 2019):
     print("progress = %" + str(games/2670*100))
@@ -53,29 +43,19 @@ for year in range (2009, 2019):
     url = "http://www.nfl.com/schedules/" + str(year) + "/POST" 
     page = scraper.go(url)
     
-    #all the different game types for postseason
-    divs = page.find_all('div',{"class":      "schedules-list-content post type-wc pro-legacy"})
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post primetime type-wc pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post type-div pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post type-con pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post type-sb pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable primetime type-wc pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable type-wc pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable type-div pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable primetime type-div pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable type-con pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable type-sb pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable primetime type-sb pro-legacy"}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable type-sb "}))
-    divs.extend( page.find_all('div',{"class":"schedules-list-content post expandable primetime type-sb "}))
+    #get all game class
+    divs = page.find_all('div',{"class": "schedules-list-content"})
     
     for div in divs:
-        games += 1
-        gameids.append(div['data-gameid'])
-
+        #check to make sure not pro bowl
+        if div["class"][-2] != "type-pro":
+            games += 1
+            gameids.append(div['data-gameid'])
+        
 #check that i got all games
 #should be 2670 = (32(teams) x 16(games) / 2(for repeats) + 11(postseason))*10(seasons)
-print(len(gameids))
+print(len(gameids)) 
+
 
 #
 # Download all game json files
@@ -94,4 +74,3 @@ for gameid in gameids:
     f = open("./GameData/game_"+gameid+".json","w")
     f.write(json.dumps(data))
     f.close()
-
